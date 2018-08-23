8850212d00f61e10e57bce6e83f7dd507cb5a249
ZCS2016 <basketbasket0@126.com>
2018-08-23 17:34:32 +0800

    add EurekaServer,RibbonTest

---
+++ RibbonTest/src/main/java/com/example/RibbonTest/RibbonTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.RibbonTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTestApplication.class, args);
+	}
+}
