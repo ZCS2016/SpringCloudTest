61f9d29db2b65225e7188769eae4c2232f647e8a
ZCS2016 <basketbasket0@126.com>
2018-08-24 13:39:45 +0800

    add Zuul

---
+++ ZuulTest/src/main/java/com/example/ZuulTest/ZuulTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.ZuulTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulTestApplication.class, args);
+	}
+}
